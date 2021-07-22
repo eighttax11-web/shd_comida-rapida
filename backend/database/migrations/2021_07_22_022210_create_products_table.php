fc83391bc608022358acf044cd8774ffc079370b
Cristian <eighttax11@gmail.com>
2021-07-21 21:31:44 -0500

    Se añadieron los campos que tendrá nuestra tabla de productos en la migración

--- backend/database/migrations/2021_07_22_022210_create_products_table.php
+++ backend/database/migrations/2021_07_22_022210_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedDecimal('price', $precision = 8, $scale = 2);
+            $table->string('image');
             $table->timestamps();
         });
     }

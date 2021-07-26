3ae95c7cde07d5b1afe1e19abcc25dea91f639a1
eduardo <aduardo@gmail.com>
2021-07-26 17:36:07 -0500

    Edición del componente ¿quiénes somos?

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
 
 const routes: Routes = [
   {
@@ -8,6 +9,9 @@ const routes: Routes = [
   },
   {
     path: 'inicio', component: HomeComponent
+  },
+  {
+    path: 'info', component: InfoComponent
   },
   {
     path: '**', component: HomeComponent

3057abb3a2eaeb52b5e1a4b96eec319c65e30fbb
Cristian <eighttax11@gmail.com>
2021-08-01 20:31:56 -0500

    Creación de la ruta que dirige a la sección de contacto y mapa

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { InfoComponent } from './components/info/info.component';
+import { MapComponent } from './components/map/map.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
@@ -16,6 +18,12 @@ const routes: Routes = [
   },
   {
     path: 'products', component: ProductsComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: 'map', component: MapComponent
   },
   {
     path: '**', component: HomeComponent

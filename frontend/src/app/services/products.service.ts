82a67f125e7280800379f7203c6b623822edc5b9
eduardo <aduardo@gmail.com>
2021-08-01 19:37:40 -0500

    Inyección del módulo http y creación de una interface para los productos

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const apiUrl: string = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  
+}
+
+export interface Product {
+  id:          number;
+  name:        string;
+  description: string;
+  price:       string;
+  image:       string;
+  created_at:  Date;
+  updated_at:  Date;
 }
